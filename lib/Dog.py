ac5a2861acf62d16238da837073e3ebf83f2ff7b
Elijah1123 <elijahmzalendo659@gmail.com>
2026-05-06 09:38:08 +0300

    Instance

---
+++ lib/Dog.py
@@ -0,0 +1,19 @@
+class Dog:
+    def bark(self):
+        print("Woof !")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def eat(self):
+        print("The dog is eating")    
+
+fido = Dog()
+fido.bark()
+fido.sit()
+fido.eat()
+
+
+
+
+
---
+++ lib/Person.py
@@ -0,0 +1,13 @@
+class Person:
+    def talk(self):
+        print("The person is walking")
+
+    def learn(self):
+        print("Mzalendo is Learning")
+
+
+
+
+Mzalendo = Person()
+Mzalendo.talk()
+Mzalendo.learn()
